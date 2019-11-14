e71b76a299f82c6ff4d3688cb4bf0b4e735838bf
Frederick Jaime <fjaime@rauxa.com>
2019-11-13 21:37:39 -0500

    setting up post thru click.

--- assets/src/js/components/form.test.js
+++ assets/src/js/components/form.test.js
@@ -3,12 +3,29 @@ export default function () {
 
 
     let respForm = document.querySelector('.responsys-test form');
+    let respSubmitBtn = document.querySelector('.submit-btn');
+    let respSubmitted = document.querySelector('.submitted-params');
+    
 
 
 
-    respForm.addEventListener('submit', (e) => {
+
+    respSubmitBtn.addEventListener('click', (e) => {
         e.preventDefault();
-        alert('test');
+        let customerId = document.querySelector('input[name="id"]');
+        let offerCode = document.querySelector('input[name="offer"]');
+        let offerAuth = document.querySelector('input[name="auth"]');
+
+
+        let respAction = `https://ifly.alaskaair.com/pub/sf/ResponseForm?CUSTOMER_ID_=${customerId.value}&OFFER_CODE=${offerCode.value}&OFFER_AUTHORIZATION=${offerAuth.value}`;
+
+
+        var request = new XMLHttpRequest();
+        request.open('POST', respAction, true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.send();
+
+        respSubmitted.innerHTML = respAction;
     });
 
 
