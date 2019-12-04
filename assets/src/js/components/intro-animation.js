694c3a64e509b0fdcc4ec6c59a0a8601dbeab078
Raymond Richards-McNaught <rrichardsmcnaught@6ZPU-KMVJ.local>
2019-12-04 13:29:39 -0500

    ASA-27 randomized intro fade animation.

--- assets/src/js/components/intro-animation.js
+++ assets/src/js/components/intro-animation.js
@@ -1,4 +1,5 @@
 import {gsap, TweenMax, TimelineMax, Power, Linear} from 'gsap';
+import { AST_SymbolMethod } from 'terser';
 
 export default function () {
   //Vars
@@ -12,14 +13,12 @@ export default function () {
 	let time = 1,
 		//Title Cards
 		titleCard = document.querySelector('.card-one-title'),
-		/*titleCardA = document.querySelector('.card-one-a'),
-		titleCardB = document.querySelector('.card-one-b'),
 		//Left Cards
 		cardleftone = document.querySelector('.card-left-one'),
 		cardlefttwo = document.querySelector('.card-left-two'),
 		//Right Cards
 		cardrightone = document.querySelector('.card-right-one'),
-		cardrighttwo = document.querySelector('.card-right-two'),*/
+		cardrighttwo = document.querySelector('.card-right-two'),
 		//
 		tl = gsap.timeline({repeat: 0, repeatDelay: 0});
 
@@ -27,7 +26,7 @@ export default function () {
 		//Fade in Title-Card and other Cards
 		tl
 		.to(titleCard, time,{opacity:1, delay:time-0.5, ease:Linear})
-		.staggerTo(allCards, .25,{opacity:1, delay:time-0.5, ease:Linear},.05,'0.25');//.05, = stagger amount//'0.25' time between animation for cards
+		.staggerTo(allCards, time,{opacity:1, delay:Math.random() * time, ease:Linear},.05,'0.25');//.05, = stagger amount//'0.25' time between animation for cards
 		console.log(allCards);
 }
 
