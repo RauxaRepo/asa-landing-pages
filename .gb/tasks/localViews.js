5e5dc85e8f8c92360ee2d8d24402c366eab61422
Frederick Jaime <fjaime@rauxa.com>
2019-12-05 06:22:56 -0500

    added html cleaner into build : stripping out comments on build.

--- .gb/tasks/localViews.js
+++ .gb/tasks/localViews.js
@@ -1,10 +1,12 @@
 const { src, dest } = require('gulp');
+const removeHtmlComments = require('gulp-remove-html-comments');
 const config = require('../config');
 
 export function localViews() {
   let frameworkBuild = `${config.local.viewsDir}`;
   
   return src(frameworkBuild)
+  .pipe(removeHtmlComments())
   .pipe(dest(config.localDir));
   
 }
