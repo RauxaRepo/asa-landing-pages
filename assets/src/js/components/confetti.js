46900697888a9a9d554ec1fa7cb270a8d0feca68
Raymond Richards-McNaught <rrichardsmcnaught@6ZPU-KMVJ.local>
2019-12-13 11:21:54 -0500

    confetti Rain built.

--- assets/src/js/components/confetti.js
+++ assets/src/js/components/confetti.js
@@ -1,35 +1,50 @@
 import {gsap, TweenMax, TimelineMax, Power, Linear, Quad} from 'gsap';
 import getBreakpoint from './breakpoints';
-import random from './draggable-cards';
 
 export default function () {
 
-    //VVRS
+    //VARS
     let wrapper = document.querySelector('.confetti-container'),
       width = window.innerWidth,
       height = window.innerHeight,
-      colorShuffle = (array) => array.sort(() => Math.random() - 0.5),
       colorArr = ['#55dded','#5599e0','#516673','#aaccdd','#41bbde','#41bbde','#ffffff','#f00000', '#ffff00'],
-      num = 200;
+      confettiArr = [],
+      num = 1000;
 
 
 		//CONFETTI
-		function confettiSpread(){
+		function confettiBuild(){
       for (let i=0; i<num; i++){
         let confetti = document.createElement('div');
+        confettiArr.push(confetti);
         wrapper.appendChild(confetti);
         confetti.style.position = 'absolute';
         confetti.style.backgroundColor = colorArr[Math.floor(Math.random() * colorArr.length)];
-				//confetti.style.transformOrigin = '50% 50%';
         confetti.style.height = Math.random() * 20 + 'px';
         confetti.style.width = Math.random() * 10 + 'px';
-        gsap.set(confetti,{x:Math.random() * width, y:Math.random() * height, rotation:Math.random() *45, scale: Math.random() * 1.2, skew: Math.random() * 30});
-				//console.log(' confetti ', confetti);
+        gsap.set(confetti,{x:Math.random() * width, y:Math.random() * height - (height + 10), rotation:Math.random() *45, scale: Math.random() * 1.2});
+				//console.log(' confetti ', i);
 			}
     }
 
+    function confettiRain(){
+      confettiArr.forEach(function(item,index){
+				//randomize confetti z index
+				item.style.zIndex = Math.floor( Math.random() * 2 );
+        gsap.to(item, {
+          duration: 3,
+          delay: .005 * index, 
+          top: '+=210vh', 
+          ease:'sine.in'
+        })
+				
+			})
+
+    }
+
 		//RUN FUNCTIONS
-    confettiSpread();
+    confettiBuild();
+    confettiRain();
         
 }
 
